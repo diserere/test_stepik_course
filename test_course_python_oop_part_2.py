4e3abb3bafd5206e5fd4b03a09f8987713f186fd
Diserere <diserere@.ya.ru>
2026-03-10 03:17:22 +0300

    1.3 Строковое представление: __str__ и __repr__. Задача 1: Реализация __str__

--- test_course_python_oop_part_2.py
+++ test_course_python_oop_part_2.py
@@ -1,1 +1,22 @@
 from icecream import ic  # noqa: F401
+
+
+
+def test_magic_str():
+    """
+    1.3 Строковое представление: __str__ и __repr__
+    
+    Задача 1: Реализация __str__
+    """
+    class Book:
+        def __init__(self, title, author):
+            self.title = title
+            self.author = author
+
+        def __str__(self):
+            return f'"{self.title}" автора {self.author}'
+
+    book = Book("1984", "George Orwell")
+
+    ic(book)
+    print(book)
