0a320e8f174028575544128ca3a5602c998be591
Diserere <diserere@.ya.ru>
2026-03-10 03:34:01 +0300

    1.3 Строковое представление: __str__ и __repr__. Задача 2: Реализация __repr__

--- test_course_python_oop_part_2.py
+++ test_course_python_oop_part_2.py
@@ -1,13 +1,19 @@
-from icecream import ic  # noqa: F401
-
+from icecream import ic
 
 
 def test_magic_str():
     """
-    1.3 Строковое представление: __str__ и __repr__
-    
+    1.3 Строковое представление: __str__ и __repr__.
+
     Задача 1: Реализация __str__
-    """
+
+    Условие:
+        Создайте класс Book.
+        1. В __init__ он должен принимать title и author.
+        2. Реализуйте метод __str__ так, чтобы он возвращал строку в формате: "[title]" автора [author].
+
+"""
+
     class Book:
         def __init__(self, title, author):
             self.title = title
@@ -20,3 +26,28 @@ def test_magic_str():
 
     ic(book)
     print(book)
+
+
+def test_magic_repr():
+    """
+    1.3 Строковое представление: __str__ и __repr__.
+
+    Задача 2: Реализация __repr__
+
+    Условие:
+        Создайте класс Point (Точка).
+        1. В __init__ он должен принимать x и y.
+        2. Реализуйте метод __repr__ так, чтобы он возвращал строку, имитирующую вызов конструктора, например: Point(x=10, y=20).
+    """
+    class Point:
+        def __init__(self, x, y):
+            self.x = x
+            self.y = y
+
+        def __repr__(self):
+            return f"Point(x={self.x!r}, y={self.y!r})"
+
+    point = Point(10, 20)
+
+    ic(point)
+    print(point)
