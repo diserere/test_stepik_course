90a55725b634ddf4147d4f8f99811f4bc751adb2
Diserere <diserere@.ya.ru>
2026-03-11 11:48:44 +0300

    Ch. 2.2 -Задача 2: Класс-библиотека

--- oop_python_part_2/s_2_advanced_methods/test_ch_2_2_staticmethods.py
+++ oop_python_part_2/s_2_advanced_methods/test_ch_2_2_staticmethods.py
@@ -23,6 +23,39 @@ def test_staticmethod_format_email():
     ic(Formatter.format_email(123))  # pyright: ignore[reportArgumentType]
 
 
+def test_staticmethod_library():
+    """
+    Задача 2: Класс-библиотека
+    """
+
+    class Validator:
+        @staticmethod
+        def is_positive(number: int | float):
+            return isinstance(number, (int, float)) and number > 0
+
+        @staticmethod
+        def is_even(number: int):
+            return isinstance(number, (int)) and number % 2 == 0
+
+    ic(Validator.is_positive(-1))
+    ic(Validator.is_positive(-10.5))
+    ic(Validator.is_positive(0))
+    ic(Validator.is_positive(0.1))
+    ic(Validator.is_positive(1))
+    ic(Validator.is_positive("1"))  # pyright: ignore[reportArgumentType]
+    ic(Validator.is_even(-2))
+    ic(Validator.is_even(-1))
+    ic(Validator.is_even(-0.2))  # pyright: ignore[reportArgumentType]
+    ic(Validator.is_even(-0.1))  # pyright: ignore[reportArgumentType]
+    ic(Validator.is_even(0))
+    ic(Validator.is_even(0.1))  # pyright: ignore[reportArgumentType]
+    ic(Validator.is_even(0.2))  # pyright: ignore[reportArgumentType]
+    ic(Validator.is_even(1))
+    ic(Validator.is_even(2))
+    ic(Validator.is_even("1"))  # pyright: ignore[reportArgumentType]
+    ic(Validator.is_even("2"))  # pyright: ignore[reportArgumentType]
+
+
 def test_():
     """
     docstring.
