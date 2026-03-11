9a90d6645576e4376ddab5024cd787f1ec213cc8
Diserere <diserere@.ya.ru>
2026-03-11 05:41:49 +0300

    Chapter 1.6 - Задача 2: Фиксированный набор атрибутов

--- oop_python_part_2/s_1_magic_methods_and_properties/test_ch_1_6_slots.py
+++ oop_python_part_2/s_1_magic_methods_and_properties/test_ch_1_6_slots.py
@@ -39,3 +39,41 @@ def test_slots():
         ic(e)
     finally:
         ic(hasattr(p, "i"))
+
+
+def test_fixed_attributes_list():
+    """
+    1.6 __slots__: Оптимизация памяти и производительности
+
+    Задача 2: Фиксированный набор атрибутов
+
+    Условие:
+    - Одним из главных следствий использования __slots__ является то, что вы больше не можете добавлять к экземпляру новые атрибуты, которые не были заранее объявлены в __slots__. Это делает структуру объекта более строгой и предсказуемой.
+
+    Вам нужно:
+    1. Создать класс User.
+    2. На уровне класса определить __slots__ и указать в нем только один разрешенный атрибут — 'username'.
+    3. Создать метод __init__(self, username), который устанавливает значение для self.username.
+
+    Шаблон кода попытается создать объект вашего класса, а затем добавить к нему новый атрибут email. Тест будет пройден, если эта попытка вызовет ожидаемую ошибку AttributeError, подтверждая, что __slots__ работает правильно.
+    """
+
+    class User:
+        __slots__ = ["username"]
+
+        def __init__(self, username) -> None:
+            self.username = username
+
+        def __repr__(self):
+            return f"{self.__class__.__name__}(username={self.username!r})"
+
+    user = User("John")
+    ic(user)
+    ic(hasattr(user, "__dict__"))
+
+    try:
+        user.email = "john@ya.ru"  # pyright: ignore[reportAttributeAccessIssue]
+    except Exception as e:
+        ic(e)
+    finally:
+        ic(hasattr(user, "email"))
