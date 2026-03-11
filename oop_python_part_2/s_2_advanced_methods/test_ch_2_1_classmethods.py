72c83c783f4de6af43783470fd8c524327abaedf
Diserere <diserere@.ya.ru>
2026-03-11 09:05:23 +0300

    Ch. 2.1 - Задача 1: Простой @classmethod

--- oop_python_part_2/s_2_advanced_methods/test_ch_2_1_classmethods.py
+++ oop_python_part_2/s_2_advanced_methods/test_ch_2_1_classmethods.py
@@ -1,7 +1,30 @@
 """2.1 Методы класса (@classmethod)"""
-# ruff: noqa: F401
 
 from icecream import ic
+
+
+def test_simple_classmethod():
+    """
+    Задача 1: Простой @classmethod
+    """
+
+    class GameCharacter:
+        def __init__(self, name, level) -> None:
+            self.name = name
+            self.level = level
+
+        @classmethod
+        def create_default_character(cls):
+            return cls("Guest", 1)
+
+        def __repr__(self):
+            variables = [f"{k}={v!r}" for k, v in vars(self).items()]
+            return f"{type(self).__name__}({', '.join(variables)})"
+
+    char_custom = GameCharacter("Frodo", 10)
+    ic(char_custom)
+    char_default = GameCharacter.create_default_character()
+    ic(char_default)
 
 
 def test_():
