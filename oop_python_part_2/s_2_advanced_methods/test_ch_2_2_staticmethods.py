f103d2d55e1cfff287231a7f0e0a4cf6a8270d3b
Diserere <diserere@.ya.ru>
2026-03-11 11:31:26 +0300

    Ch. 2.2 -Задача 1: Простая утилита

--- oop_python_part_2/s_2_advanced_methods/test_ch_2_2_staticmethods.py
+++ oop_python_part_2/s_2_advanced_methods/test_ch_2_2_staticmethods.py
@@ -1,7 +1,26 @@
 """2.2 Статические методы (@staticmethod)"""
-# ruff: noqa: F401
 
 from icecream import ic
+
+
+def test_staticmethod_format_email():
+    """
+    Задача 1: Простая утилита
+    """
+
+    class Formatter:
+        @staticmethod
+        def format_email(email: str):
+            if isinstance(email, str) and "@" in email:
+                return email.lower().strip()
+
+    ic(Formatter.format_email("   My_EMAil@dot.coM"))
+    ic(Formatter.format_email("   My_EMAil@DOT.coM   "))
+    ic(Formatter.format_email("   My_EMAil @ DOT.coM   "))
+    ic(Formatter.format_email("   \nMy_EMAil@dot.coM"))
+    ic(Formatter.format_email("   \nMy_EMAil DOT.coM   "))
+    ic(Formatter.format_email(["my_email@dot.com"]))  # pyright: ignore[reportArgumentType]
+    ic(Formatter.format_email(123))  # pyright: ignore[reportArgumentType]
 
 
 def test_():
