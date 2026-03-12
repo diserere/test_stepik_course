ea45e8d875c8fc96381dc56839c4889891728dd8
Diserere <diserere@.ya.ru>
2026-03-12 10:46:17 +0300

    Глава 3.1. Задача 1: Простое множественное наследование

--- oop_python_part_2/s_3_architecture_and_relations/test_ch_3_2_multiple_inheritance_mixins.py
+++ oop_python_part_2/s_3_architecture_and_relations/test_ch_3_2_multiple_inheritance_mixins.py
@@ -1,7 +1,28 @@
 """3.2 Множественное наследование и Миксины"""
-# ruff: noqa: F401
 
 from icecream import ic
+
+
+def test_multiple_inheritance():
+    """
+    Задача 1: Простое множественное наследование
+    """
+
+    class Swimmer:
+        @staticmethod
+        def swim():
+            return "Я плыву"
+
+    class Walker:
+        @staticmethod
+        def walk():
+            return "Я иду"
+
+    class Amphibian(Swimmer, Walker): ...
+
+    frog = Amphibian()
+    ic(frog.swim())
+    ic(frog.walk())
 
 
 def test_():
