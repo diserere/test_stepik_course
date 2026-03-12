ec71e467a3f7fcf3e827c71707ec196fd97d9311
Diserere <diserere@.ya.ru>
2026-03-12 10:59:05 +0300

    Глава 3.1. Задача 2: Порядок наследования (MRO)

--- oop_python_part_2/s_3_architecture_and_relations/test_ch_3_2_multiple_inheritance_mixins.py
+++ oop_python_part_2/s_3_architecture_and_relations/test_ch_3_2_multiple_inheritance_mixins.py
@@ -25,6 +25,32 @@ def test_multiple_inheritance():
     ic(frog.walk())
 
 
+def test_inheritance_order():
+    """
+    Задача 2: Порядок наследования (MRO)
+    """
+
+    class Radio:
+        @staticmethod
+        def play():
+            return "Радио играет"
+
+    class Speaker:
+        @staticmethod
+        def play():
+            return "Колонка играет"
+
+    class Boombox(Radio, Speaker):
+        def __repr__(self):
+            variables = [f"{k}={v!r}" for k, v in vars(self).items()]
+            return f"{type(self).__name__}({', '.join(variables)})"
+
+    boombox = Boombox()
+    ic(boombox)
+    ic(boombox.play())
+    ic(boombox.__class__.__mro__)
+
+
 def test_():
     """
     docstring.
