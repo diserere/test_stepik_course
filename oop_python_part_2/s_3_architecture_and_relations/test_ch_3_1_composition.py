e72e1773a607c80a746e2fa5879dd84479a7eda9
Diserere <diserere@.ya.ru>
2026-03-12 06:12:19 +0300

    Глава 3.1. Задача 2: Делегирование вызова

--- oop_python_part_2/s_3_architecture_and_relations/test_ch_3_1_composition.py
+++ oop_python_part_2/s_3_architecture_and_relations/test_ch_3_1_composition.py
@@ -26,6 +26,36 @@ def test_simple_relation_has_a():
     ic(person)
 
 
+def test_call_delegation():
+    """
+    Задача 2: Делегирование вызова
+    """
+
+    class CPU:
+        @staticmethod
+        def calculate():
+            return "Вычисления..."
+
+        def __repr__(self):
+            variables = [f"{k}={v!r}" for k, v in vars(self).items()]
+            return f"{type(self).__name__}({', '.join(variables)})"
+
+    class Computer:
+        def __init__(self) -> None:
+            self.cpu = CPU()
+
+        def __repr__(self):
+            variables = [f"{k}={v!r}" for k, v in vars(self).items()]
+            return f"{type(self).__name__}({', '.join(variables)})"
+
+        def run(self):
+            return self.cpu.calculate()
+
+    c = Computer()
+    ic(c)
+    ic(c.run())
+
+
 def test_():
     """
     docstring.
