5eb0c7ffc6f447b33fc65eec360a1e443b74cc63
Diserere <diserere@.ya.ru>
2026-03-14 12:39:09 +0300

    Глава 4.2 Задача 1: Простой датакласс

--- oop_python_part_2/s_4_modern_oop_tools/test_ch_4_2_dataclasses.py
+++ oop_python_part_2/s_4_modern_oop_tools/test_ch_4_2_dataclasses.py
@@ -1,7 +1,29 @@
 """4.2 Датаклассы (@dataclass)"""
-# ruff: noqa: F401
+
+from dataclasses import dataclass
 
 from icecream import ic
+
+
+def test_simple_dataclass():
+    """
+    Задача 1: Простой датакласс
+    """
+
+    @dataclass
+    class Point:
+        x: int
+        y: int
+
+    p = Point(1, 2)
+    print(p)
+    ic(p)
+
+    ic(Point(1, 2))
+    ic(p == Point(1, 2))
+
+    ic(p == Point(3, 2))
+    ic(p == Point(1, 1))
 
 
 def test_():
